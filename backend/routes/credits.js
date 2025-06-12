e538ed504ef7708ca4e12623eee7a97486ab16dd
mehrin <mehrinshamim4869@gmail.com>
2025-06-12 22:08:37 +0530

    cleared out

--- backend/routes/credits.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addCredits, getCredits } = require('../controllers/creditsController');
-const { verifyAuth } = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.post('/add', verifyAuth, addCredits);
-router.get('/', verifyAuth, getCredits);
-
-module.exports = router;
